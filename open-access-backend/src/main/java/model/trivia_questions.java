23655c9f6c0a84eab91fed65ddda54d14ba4c0f2
Sheree <sheree.edmund@gmail.com>
2022-01-27 13:00:46 -0500

    uploading backend

---
+++ open-access-backend/src/main/java/model/trivia_questions.java
@@ -0,0 +1,82 @@
+package model;
+
+import  java.util.Arrays
+import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "trivia_questions")
+
+public class trivia_questions {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String question;
+
+    @Column
+    private String incorrect;
+
+    @Column
+    private String correct;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "answer_id")
+    private Answer answer;
+
+
+
+
+
+public trivia_questions() {
+
+	public trivia_questions((String question, String[] incorrect, String correct) {
+        this.question = question;
+        this.incorrect = incorrect;
+        this.correct = correct;
+
+    }
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String[] getIncorrect() {
+        return incorrect;
+    }
+
+    public void setIncorrect(String[] incorrect) {
+        this.incorrect = incorrect;
+    }
+
+    public String[] getCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(String[] correct) {
+        this.correct = correct;
+
+}
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Question [id=" + id + ", question="
+                + question + ", incorrect=" + Arrays
+                .toString(incorrect) + ", correct="
+                + correct + "]";
+}
+}
--- server.js
+++ server.js
@@ -6,3 +6,5 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/<ga-capstone>/index.html'));});
 app.listen(process.env.PORT || 8080);
+
+//for commit"
