5f9974c274235b302c65e8aebf6fc2dc640a4709
Sheree <sheree.edmund@gmail.com>
2022-01-28 12:13:50 -0500

    updated front end css

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/<ga-capstone>'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/<ga-capstone>/index.html'));});
-app.listen(process.env.PORT || 8080);
 
-//for commit"
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/ga-capstone'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/ga-capstone/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
