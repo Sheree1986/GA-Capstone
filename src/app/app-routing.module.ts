2f6900d4a841ce4661d8f38c91d033fc106c13af
Sheree <sheree.edmund@gmail.com>
2022-01-30 12:40:57 -0500

    working on design of frontend

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent
+  },
+];
+//   {
+//     path: 'cities',
+//     component: PhoneBookComponent
+//   },
+//   {
+//     path: '**',
+//     component: FourOhFourComponent
+//   }
+// ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+   
+  
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomepageComponent } from 'src/app/pages/homepage/homepage.component';
+import { HomepageComponent } from './homepage/homepage.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-   HomepageComponent
+    HomepageComponent,
+    NavbarComponent
 
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    Ng2SearchPipeModule,
+    HttpClientModule,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
