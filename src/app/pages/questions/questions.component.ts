1ca0a3892178313f9919b9e4c5b0b7e82c6cbadb
Sheree <sheree.edmund@gmail.com>
2022-02-03 23:48:32 -0500

    working on quiz qeusitons

--- src/app/pages/questions/questions.component.ts
+++ src/app/pages/questions/questions.component.ts
@@ -14,8 +14,60 @@ export class QuestionsComponent implements OnInit {
   // private moonURL = 'https://open-access-backend.herokuapp.com/api/question/';
 // let's make the URL routes available to class 
 
-  questions: any;
-  qId: any;
+
+public questions: any = [];
+public currentQuestion: number = 0;
+public currentA: number = 0;
+public score: number=0;
+correct: number = 0;
+wrong: number = 0;
+
+
+constructor(private quizService: QuizService) { }
+
+ngOnInit(): void {
+  this.getAllQuestions();
+}
+getAllQuestions(){
+this.quizService.getQuestion().subscribe(res => {
+this.questions = res;
+
+})
+
+}
+nextQuestion(){
+
+  this.currentQuestion++;
+  this.currentA++;
+}
+previousQuestion(){
+  this.currentQuestion--;
+this.currentA++;
+}
+answer(currentQ:number,option:any){
+  if (option.correct) {
+  this.score +=10
+  this.correct ++;
+  this.currentQuestion++;
+} else {
+
+  this.score-=10;
+  this.currentQuestion++;
+  this.wrong ++; 
+}
+}
+restart(){
+  this.getAllQuestions()
+  this.score=0;
+  this.currentQuestion=0;
+  
+}
+}
+
+
+
+
+  // qId: any;
   // clickMe () {
   //   this.http
   //   .get(`https://open-access-backend.herokuapp.com/api/question/1`)
@@ -25,25 +77,13 @@ export class QuestionsComponent implements OnInit {
 // let's make the URL routes available to class
 
 
-constructor(private quizService: QuizService) { }
-
-ngOnInit(): void {
-
-this.quizService.getQuestion().subscribe(data => {
-this.questions = data;
-})
 // this.quizService.getQuestionId().subscribe(data => {
 //   this.qId = data;
 // })
-}
-
-}
-
-  // this.quizService.getQuestions().subscribe((questions: Question[]) => {
-  //   this.questions = questions;
+ // this.quizService.getQuestions().subscribe((questions: Question[]) => {
+//   this.questions = questions;
   
-  // this.quizService.getQuestions().subscribe(data => {
-    // this.questions =  data;
+// this.quizService.getQuestions().subscribe(data => { // this.questions =  data;
 //   })
 // }
 // }
